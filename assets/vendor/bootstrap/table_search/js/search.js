62b8ef48596e3491261c88606e4c32e571bd5a00
soumuyadebdutta <soumyadebdutta123@gmail.com>
2020-08-10 10:43:23 +0530

    This is my Primary Website
    Wow,This the best.

---
+++ assets/vendor/bootstrap/table_search/js/search.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#table-class-1').DataTable();
+} );
+$(document).ready(function() {
+    $('#table-class-2').DataTable();
+} );
+$(document).ready(function() {
+    $('#table-class-3').DataTable();
+} );
+$(document).ready(function() {
+    $('#table-class-4').DataTable();
+} );
+$(document).ready(function() {
+    $('#table-class-5').DataTable();
+} );
